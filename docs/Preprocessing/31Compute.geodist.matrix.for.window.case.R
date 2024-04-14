bea868e063b0d1efbdd178950bae496f36a82388
leninrafaelrierasegura <leninrafael.rierasegura@kaust.edu.sa>
2024-04-14 21:38:23 +0300

    update up to model 4

---
+++ docs/Preprocessing/31Compute.geodist.matrix.for.window.case.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(here)
+
+# loading the data
+load(here("Data_files/data_on_graph_with_covariates_no_consecutive_zeros_partialtomtom.RData"))
+load(here("Graph_objects/graph_construction_28_03_2024partialtomtomwhichlonglatsf.RData"))
+
+aux = data_on_graph_with_covariates |>
+  rename(distance_on_edge = .distance_on_edge, edge_number = .edge_number) |>
+  as.data.frame() |>
+  select(edge_number, distance_on_edge)
+
+distmatrix = graph$compute_geodist_PtE(PtE = aux, 
+                                       normalized = TRUE, 
+                                       include_vertices = FALSE)
+
+
+save(distmatrix, file = here("Models_output/distmatrix.RData"))
