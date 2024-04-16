b01e7ea69224d4c051dc6aae94fc2e7d740315eb
leninrafaelrierasegura <leninrafael.rierasegura@kaust.edu.sa>
2024-04-17 01:36:51 +0300

    update on model 9

--- Preprocessing/31Compute.geodist.matrix.for.window.case.R
+++ Preprocessing/31Compute.geodist.matrix.for.window.case.R
@@ -8,19 +8,25 @@ load(here("Graph_objects/graph_construction_28_03_2024partialtomtomwhichlonglats
 aux = data_on_graph_with_covariates |>
   rename(distance_on_edge = .distance_on_edge, edge_number = .edge_number) |>
   as.data.frame() |>
-  dplyr::select(edge_number, distance_on_edge)
-# 
-# distmatrix = sf_graph$compute_geodist_PtE(PtE = aux, 
-#                                        normalized = TRUE, 
-#                                        include_vertices = FALSE)
-# 
-# 
-# save(distmatrix, file = here("Models_output/distmatrix.RData"))
+  dplyr::select(edge_number, distance_on_edge, .group)
+
+distmatrixlist = list()
+
+for (i in 1:4) {
+  distmatrixlist[[i]] = sf_graph$compute_geodist_PtE(PtE = aux %>% 
+                                                       filter(.group == as.character(i)) %>% 
+                                                       dplyr::select(-.group),
+                                                     normalized = TRUE,
+                                                     include_vertices = FALSE)
+}
+
+
+save(distmatrixlist, file = here("Models_output/distmatrix.RData"))
 
 # Warning message:
 #   In system.time({ :
 #       Duplicated locations were found when computing geodist. The returned values are given for unique locations.
 #     
     
-load(here("Models_output/distmatrix.RData"))   
-nrow(unique(aux))
+# load(here("Models_output/distmatrix.RData"))   
+# nrow(unique(aux))
